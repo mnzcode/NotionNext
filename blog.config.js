4d3e03ebda237b74126895ee79091f9e7447e4bb
mnzcode <118622445+mnzcode@users.noreply.github.com>
2023-03-11 12:08:29 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,7 +14,7 @@ const BLOG = {
 
   AUTHOR: '挨踢牛马', // 您的昵称 例如 tangly1024
   BIO: '学挨踢，牛马程序员🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: 'https://notion-next-omega-jet.vercel.app', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
